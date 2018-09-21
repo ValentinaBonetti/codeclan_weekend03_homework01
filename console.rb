c0351cee90751445f8c4f0a443feec972b735937
Valentina Bonetti <valentina.bonetti@strath.ac.uk>
2018-09-21 14:35:40 +0100

    Add film class CRUD functionalities

--- console.rb
+++ console.rb
@@ -4,3 +4,10 @@ require_relative("models/screening")
 require_relative("models/ticket")
 
 require("pry")
+
+
+film1 = Film.new({"title" => "Priscilla queen of the desert", "price" => 10})
+film1.save
+
+binding.pry
+nil 
---
+++ models/film.rb
@@ -0,0 +1,34 @@
+require_relative("../db/sql_runner")
+
+class Film
+
+  attr_reader :id
+  attr_accessor :title, :price #check: do we need total access?
+
+  def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @title = options["title"]
+    @price = options["price"]
+  end
+
+  def save()
+    sql = "INSERT INTO films (title,price) VALUES ($1,$2) RETURNING *"
+    values = [@title,@price]
+    film = SqlRunner.run(sql,values).first
+    @id = film["id"].to_i
+  end
+
+  def delete()
+    sql = "DELETE FROM films WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql,values)
+  end
+
+  def update()
+    sql = "UPDATE films SET (title,price) = ($1,$2)
+           WHERE id = $3"
+    values = [@title,@price,@id]
+    SqlRunner.run(sql,values)
+  end
+
+end
